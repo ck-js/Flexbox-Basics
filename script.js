e79fe216ab3659aecfe04594820f44dfba055793
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-05 18:34:37 +0700

    Add the other flex direction class and stored the flex container in a variable, create functions for each event handler and called the buttons using event listeners. Introduced a bug when a class is added we cnt select the other classes using the buttons

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const el = document.querySelector(".flex-container-3");
 const rowButton = document.querySelector(".row-button")
+const rowReverseButton = document.querySelector(".row-reverse-button");
+const columnButton = document.querySelector(".column-button");
+const columnReverseButton = document.querySelector("column-reverse-button");
 
 
 function changeRow() {
-    el.classList.toggle("flex-row");
+    el.classList.add("flex-row");
 }
 
-rowButton.addEventListener("click", changeRow);
+function changeRowReverse() {
+    el.classList.add("flex-row-reverse");
+}
 
+function changeColumn() {
+    el.classList.add("flex-column");
+}
+
+
+
+rowButton.addEventListener("click", changeRow);
+rowReverseButton.addEventListener("click", changeRowReverse);
+columnButton.addEventListener("click", changeColumn);
 
