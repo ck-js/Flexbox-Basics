7398b1f6a5dd9cb1ccef6f070c48d7adb9e229e1
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-06 20:19:06 +0700

    Read thrugh sample code to better understand where im going wrong, I think it has to do with the active class and its configuation

--- script.js
+++ script.js
@@ -5,28 +5,60 @@ const columnButton = document.querySelector(".column-button");
 const columnReverseButton = document.querySelector(".column-reverse-button");
 
 
-function changeRow() {
+function changeRow(event) {
     el.classList.toggle("flex-row");
+    
+    const clickedButton = event.target;
+
+    // Toggle the class "active" on all buttons except the clicked one
+    if (clickedButton !== rowButton) {
+        rowButton.classList.remove("active");
+      }
+      if (clickedButton !== rowReverseButton) {
+        rowReverseButton.classList.remove("active");
+      }
+      if (clickedButton !== columnButton) {
+        columnButton.classList.remove("active");
+      }
+      if (clickedButton !== columnReverseButton) {
+        columnReverseButton.classList.remove("active");
+      }
+      
+      // Toggle the class "active" on the clicked button
+      clickedButton.classList.toggle("active");
+    
+    
+
+
 
 }
 
-function changeRowReverse() {
+function changeRowReverse(event) {
     el.classList.toggle("flex-row-reverse");
 
 }
 
-function changeColumn() {
+function changeColumn(event) {
     el.classList.toggle("flex-column");
 }
 
 function changeColumnReverse() {
-    el.classList.toggle("flex-container-3");
-    
+    el.classList.toggle("flex-column-reverse");
+
 }
-
-
 
 rowButton.addEventListener("click", changeRow);
 rowReverseButton.addEventListener("click", changeRowReverse);
 columnButton.addEventListener("click", changeColumn);
 columnReverseButton.addEventListener("click", changeColumnReverse)
+
+
+
+
+
+
+
+
+
+
+
