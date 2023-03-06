57aea605e8cae6ecfd5480559add40fe3c3f764d
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-06 15:15:13 +0700

    Fixed buttons that toggle different event handlers however still some bugs where a class wont toggle because of improper class input

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ const el = document.querySelector(".flex-container-3");
 const rowButton = document.querySelector(".row-button")
 const rowReverseButton = document.querySelector(".row-reverse-button");
 const columnButton = document.querySelector(".column-button");
-const columnReverseButton = document.querySelector("column-reverse-button");
+const columnReverseButton = document.querySelector(".column-reverse-button");
 
 
 function changeRow() {
     el.classList.toggle("flex-row");
-el.removeEventListener("click", changeRow);
 
 }
 
@@ -20,9 +19,14 @@ function changeColumn() {
     el.classList.toggle("flex-column");
 }
 
+function changeColumnReverse() {
+    el.classList.toggle("flex-container-3");
+    
+}
+
 
 
 rowButton.addEventListener("click", changeRow);
 rowReverseButton.addEventListener("click", changeRowReverse);
 columnButton.addEventListener("click", changeColumn);
-
+columnReverseButton.addEventListener("click", changeColumnReverse)
