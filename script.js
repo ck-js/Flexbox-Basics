8d11c5db95f0624c405acf5cccfb15303c517dc7
Cliffton Kaewkarn <ck@Clifftons-MacBook-Air.local>
2023-03-07 11:16:32 +0700

    Style third container buttons

--- script.js
+++ script.js
@@ -5,40 +5,20 @@ const columnButton = document.querySelector(".column-button");
 const columnReverseButton = document.querySelector(".column-reverse-button");
 
 
-function changeRow(event) {
+
+function changeRow() {
     el.classList.toggle("flex-row");
+  el.classList.add("flex-container-3");
+
     
-    const clickedButton = event.target;
-
-    // Toggle the class "active" on all buttons except the clicked one
-    if (clickedButton !== rowButton) {
-        rowButton.classList.remove("active");
-      }
-      if (clickedButton !== rowReverseButton) {
-        rowReverseButton.classList.remove("active");
-      }
-      if (clickedButton !== columnButton) {
-        columnButton.classList.remove("active");
-      }
-      if (clickedButton !== columnReverseButton) {
-        columnReverseButton.classList.remove("active");
-      }
-      
-      // Toggle the class "active" on the clicked button
-      clickedButton.classList.toggle("active");
-    
-    
-
-
-
 }
 
-function changeRowReverse(event) {
+function changeRowReverse() {
     el.classList.toggle("flex-row-reverse");
 
 }
 
-function changeColumn(event) {
+function changeColumn() {
     el.classList.toggle("flex-column");
 }
 
